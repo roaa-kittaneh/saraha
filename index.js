970d39d57ac6bf907390e899ca529b419a894acd
roaa-kittaneh <roaakittaneh2004@gmail.com>
2024-05-09 11:20:26 +0300

    edit port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 //import 'dotenv/config'
 const app=express();
-const PORT=process.env.PORT;
+const PORT=process.env.PORT ||3000;
 
 initAPP(app,express);
 app.listen(PORT,()=>{
